899a99f831ff811c61036b1036611f908de4f872
Alexander Kislitsky <akislitsky@mirantis.com>
2016-08-17 18:05:57 +0300

    Update of resource values implemented
    Now it is able to add, update, delete keys in resource values
    
    Change-Id: I26a68f40d1c3f36dfbedfd93efaf4f79e86acd85

--- tuning_box/tests/library/test_resource_values.py
+++ tuning_box/tests/library/test_resource_values.py
@@ -10,11 +10,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import itertools
+
 from tuning_box import db
 from tuning_box.tests.test_app import BaseTest
 
 
 class TestResourceValues(BaseTest):
+
+    object_url = '/environments/{0}/{1}/resources/{2}/values'
+    object_keys_url = object_url + '/keys/{3}'
 
     def test_put_resource_values_root(self):
         self._fixture()
@@ -129,3 +134,93 @@ class TestResourceValues(BaseTest):
             'http://localhost'
             '/environments/9/lvl1/val1/lvl2/val2/resources/5/values',
         )
+
+    def test_put_resource_values_not_found(self):
+        self.app.config["PROPAGATE_EXCEPTIONS"] = True
+        self._fixture()
+
+        res = self.client.put(
+            '/environments/9/lvl1/val1/resources/5/values/keys/set',
+            data={}
+        )
+        self.assertEqual(404, res.status_code)
+
+    def test_put_resource_values_set_operation_error(self):
+        self.app.config["PROPAGATE_EXCEPTIONS"] = True
+        self._fixture()
+
+        environment_id = 9
+        res_def_id = 5
+        levels = (('lvl1', 'val1'), ('lvl2', 'val2'))
+        values = {'key': 'val'}
+        self._add_resource_values(environment_id, res_def_id, levels, values)
+
+        data = [['a', 'b', 'c', 'value']]
+        obj_keys_url = self.object_keys_url.format(
+            environment_id,
+            '/'.join(itertools.chain.from_iterable(levels)),
+            res_def_id,
+            'set'
+        )
+
+        res = self.client.put(obj_keys_url, data=data)
+        self.assertEqual(409, res.status_code)
+
+    def test_put_resource_values_set(self):
+        self._fixture()
+        environment_id = 9
+        res_def_id = 5
+        levels = (('lvl1', 'val1'), ('lvl2', 'val2'))
+        values = {'key': 'val'}
+        self._add_resource_values(environment_id, res_def_id, levels, values)
+
+        obj_url = self.object_url.format(
+            environment_id,
+            '/'.join(itertools.chain.from_iterable(levels)),
+            res_def_id
+        )
+        obj_keys_url = obj_url + '/keys/set'
+
+        data = [['key', 'key_value'], ['key_x', 'key_x_value']]
+        res = self.client.put(obj_keys_url, data=data)
+        self.assertEqual(204, res.status_code)
+
+        res = self.client.get(obj_url)
+        self.assertEqual(200, res.status_code)
+        actual = res.json
+        self.assertEqual({'key': 'key_value', 'key_x': 'key_x_value'},
+                         actual)
+
+    def test_put_resource_values_delete(self):
+        self._fixture()
+        environment_id = 9
+        res_def_id = 5
+        levels = (('lvl1', 'val1'), ('lvl2', 'val2'))
+        values = {'key_0': 'val_0', 'key_1': 'val_1'}
+        self._add_resource_values(environment_id, res_def_id, levels, values)
+
+        obj_url = self.object_url.format(
+            environment_id,
+            '/'.join(itertools.chain.from_iterable(levels)),
+            res_def_id
+        )
+        obj_keys_url = obj_url + '/keys/delete'
+
+        data = [['key_0']]
+        res = self.client.put(obj_keys_url, data=data)
+        self.assertEqual(204, res.status_code)
+
+        res = self.client.get(obj_url)
+        self.assertEqual(200, res.status_code)
+        actual = res.json
+        self.assertEqual({'key_1': 'val_1'}, actual)
+
+        # Checking of not existed key deletion
+        data = [['fake_key']]
+        res = self.client.put(obj_keys_url, data=data)
+        self.assertEqual(204, res.status_code)
+
+        res = self.client.get(obj_url)
+        self.assertEqual(200, res.status_code)
+        actual = res.json
+        self.assertEqual({'key_1': 'val_1'}, actual)
